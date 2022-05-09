46a427392a2a71d22c88ee7ecc017c1bdd5c7700
waitherakamau <effencekamau@gmail.com>
2022-05-09 22:28:01 +0300

    first commit

---
+++ day-1/intro.py
@@ -0,0 +1,45 @@
+#operand 3 and 4
+
+print(3+4)
+
+print(3-4)
+
+print(3*4)
+
+print(3%4)#modulus finding remainder
+
+print(3/4)#division
+
+print(3**4)#exponential operator,equivqlent to
+
+print(3//4)#floor distribution operator ,removes the remainder
+
+                              #Strings
+
+
+print('Effence')
+
+print('the Muthandi family')
+
+print('Kenya')
+
+print('i enjoy Python because i am a pithonista')
+
+                             #Data types
+                             
+print (type(10))
+
+print (type(9.8))
+
+print (type(4-4j))
+
+print(type(['Cynthia','python','Kenya']))
+
+print(type('Effence'))
+
+print (type('Muthadi family'))
+
+print (type('Kenyae'))
+
+
+
